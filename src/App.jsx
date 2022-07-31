cc7ef21ddb435ecad5df3de5429a66334146e994
jdgc14 <joseojedapro@gmail.com>
2022-07-30 20:16:34 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Home from './components/Home'
+import "bootswatch/dist/superhero/bootstrap.min.css"
+
+function App() {
+
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  )
+}
+
+export default App
