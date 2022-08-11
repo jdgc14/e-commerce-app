8df1dd10326158196dbcee23ccc3935dc6a4288e
jdgc14 <joseojedapro@gmail.com>
2022-08-10 20:20:07 -0500

    adding og:image

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import Categories from './Categories';
 import FilterByPrice from './FilterByPrice';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const Filters = () => {
     return (
